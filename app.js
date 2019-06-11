bee4eed33cd22498e3c5f91fd679d05ed8ec7be8
jacs <wbruce0110@gmail.com>
2019-06-11 22:57:58 +0800

    add body-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // include packages
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -8,8 +9,16 @@ const port = 3000
 app.engine('handlebars', exphbs({ defultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting boy-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // routes
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('req.body', req.body)
   res.render('index')
 })
 
