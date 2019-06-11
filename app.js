902d44a10eb076becc5ca3ba435e7cc29e6b8b13
jacs <wbruce0110@gmail.com>
2019-06-11 22:47:09 +0800

    setup express app

---
+++ app.js
@@ -0,0 +1,22 @@
+// include packages
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+// setup view engine
+app.engine('handlebars', exphbs({ defultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// routes
+
+app.get('/', (req, res) => {
+  console.log('Express')
+  res.send('Hello world')
+})
+
+// listening on port 
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`)
+})
